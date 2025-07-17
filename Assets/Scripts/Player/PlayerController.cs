32a4756eb58fc4e385ba5d04d8827fc02b812739
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-07-17 21:38:11 +0500

    Character rotation added

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,9 +3,11 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
   [Header("Player speed")]
-  [SerializeField] private float _playerSpeed = 0;
+  [SerializeField] private float _playerMoveSpeed = 0;
+  [SerializeField] private float _playerRotateSpeed = 0;
   private CharacterController _characterController;
-  private Vector3 _playerInput = new();
+  private Vector3 _playerMove;
+  private float _playerRotate;
 
   private void Start()
   {
@@ -15,20 +17,27 @@ public class PlayerController : MonoBehaviour
 
   private void Update()
   {
-    _playerInput.x = GetInput<Vector2>("Move").x;
-    _playerInput.z = GetInput<Vector2>("Move").y;
-    _characterController.Move(_playerSpeed * Time.deltaTime * _playerInput);
-
+    PlayerMove();
+    PlayerRotate();
     Debug.Log(GetInput<float>("Jump"));
-  }
-
-  private Vector2 OldInput()
-  {
-    return new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
   }
 
   private T GetInput<T>(string actionName) where T : struct
   {
     return InputSystem.actions.FindAction(actionName).ReadValue<T>();
+  }
+
+  private void PlayerMove()
+  {
+    _playerMove.x = GetInput<Vector2>("Move").x;
+    _playerMove.z = GetInput<Vector2>("Move").y;
+    _playerMove = transform.TransformDirection(_playerMove); // Transform to local axis
+    _characterController.Move(_playerMoveSpeed * Time.deltaTime * _playerMove);
+  }
+
+  private void PlayerRotate()
+  {
+    _playerRotate = GetInput<Vector2>("Look").x;
+    transform.Rotate(Vector3.up, _playerRotateSpeed * Time.deltaTime * _playerRotate);
   }
 }
