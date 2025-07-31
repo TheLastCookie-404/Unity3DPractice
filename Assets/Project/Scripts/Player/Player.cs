acdd271c9234bc1eb8cbcf66cbecd2a22868e020
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-07-31 15:52:42 +0500

    Tech

--- Assets/Project/Scripts/Player/Player.cs
+++ Assets/Project/Scripts/Player/Player.cs
@@ -1,53 +1,30 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-  [Header("Player speed")]
-  [SerializeField] private float _playerDefaultSpeed = 5f;
-  [SerializeField] private float _playerSprintSpeed = 10f;
-  [SerializeField] private float _playerRotateSpeed = 30f;
-  [SerializeField] private float _playerMass = 5f;
-  [SerializeField] private float _playerJumpHeight = 1.5f;
+  [Header("Player settings")]
+  [SerializeField] private float _defaultSpeed = 5f;
+  [SerializeField] private float _sprintSpeed = 10f;
+  [SerializeField] private float _rotateSpeed = 30f;
+  [SerializeField] private float _mass = 5f;
+  [SerializeField] private float _jumpHeight = 1.5f;
   [SerializeField] private float _moveInterpolatoinFactor = 10f;
 
   private CharacterController _characterController;
-  private PlayerInputSystem _playerInputSystem;
-
-  private Vector2 _moveInput;
-  private Vector2 _lookInput;
-  private bool _sprintInput;
-  private bool _jumpInput;
-
-  private Vector3 _playerMove;
-  private float _playerMoveSpeed;
+  private Vector3 _move;
+  private float _moveSpeed;
 
   private void Awake()
   {
     _characterController = gameObject.GetOrAddComponent<CharacterController>();
-    _playerInputSystem = new PlayerInputSystem();
-  }
-
-  private void OnEnable()
-  {
-    _playerInputSystem.Enable();
-  }
-
-  private void OnDisable()
-  {
-    _playerInputSystem.Disable();
   }
 
   private void Update()
   {
-    ApplyControllerMove(_playerMove);
+    ApplyControllerMove(_move);
     Gravity();
-    GetInpup();
-
-    Move(_moveInput);
-    Jump(_jumpInput);
-    Sprint(_sprintInput);
-    Rotate(_lookInput.x);
   }
 
   private void ApplyControllerMove(Vector3 motion)
@@ -56,49 +33,41 @@ public class Player : MonoBehaviour
     _characterController.Move(motion * Time.deltaTime);
   }
 
-  public void Gravity()
+  private void Gravity()
   {
     if (!_characterController.isGrounded)
     {
       // Multyplied by Time.deltaTime twice, for downwards acceleration (gravity * mass * time^2)
-      _playerMove.y += Physics.gravity.y * _playerMass * Time.deltaTime;
+      _move.y += Physics.gravity.y * _mass * Time.deltaTime;
     }
     else
       // -0.5f is for reducing fluctuations when character is on the ground
       // thats a fix for _characterController.isGrounded
-      _playerMove.y = -0.5f;
+      _move.y = -0.5f;
   }
 
-  private void GetInpup()
+  public void Move(Vector2 motionInput)
   {
-    _moveInput = _playerInputSystem.Player.Move.ReadValue<Vector2>() * _playerMoveSpeed;
-    _lookInput = _playerInputSystem.Player.Look.ReadValue<Vector2>() * _playerRotateSpeed;
-    _jumpInput = _playerInputSystem.Player.Jump.WasPressedThisFrame();
-    _sprintInput = _playerInputSystem.Player.Sprint.IsPressed();
+    _move.x = Mathf.Lerp(_move.x, motionInput.x * _moveSpeed, _moveInterpolatoinFactor * Time.deltaTime);
+    _move.z = Mathf.Lerp(_move.z, motionInput.y * _moveSpeed, _moveInterpolatoinFactor * Time.deltaTime);
   }
 
-  private void Move(Vector2 motionInput)
+  public void Sprint(bool sprintInput)
   {
-    _playerMove.x = Mathf.Lerp(_playerMove.x, motionInput.x, _moveInterpolatoinFactor * Time.deltaTime);
-    _playerMove.z = Mathf.Lerp(_playerMove.z, motionInput.y, _moveInterpolatoinFactor * Time.deltaTime);
+    _moveSpeed = sprintInput ? _sprintSpeed : _defaultSpeed;
   }
 
-  private void Sprint(bool sprintInput)
+  public void Rotate(float rotateInput)
   {
-    _playerMoveSpeed = sprintInput ? _playerSprintSpeed : _playerDefaultSpeed;
+    transform.Rotate(Vector3.up, Time.deltaTime * rotateInput * _rotateSpeed);
   }
 
-  private void Rotate(float rotateInput)
-  {
-    transform.Rotate(Vector3.up, Time.deltaTime * rotateInput);
-  }
-
-  private void Jump(bool jumpInput)
+  public void Jump(bool jumpInput)
   {
     if (jumpInput && _characterController.isGrounded)
     {
       // This is formula for jump to a specific height (* -2f) is part of formula
-      _playerMove.y = Mathf.Sqrt(_playerJumpHeight * -2f * (Physics.gravity.y * _playerMass));
+      _move.y = Mathf.Sqrt(_jumpHeight * -2f * (Physics.gravity.y * _mass));
     }
   }
 }
--- Assets/Project/Scripts/Player/PlayerController.cs
+++ Assets/Project/Scripts/Player/PlayerController.cs
@@ -1,8 +1,47 @@
-using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
 public class PlayerController : MonoBehaviour
 {
+  private Player _player;
+  private PlayerInputSystem _playerInputSystem;
+  private Vector2 _moveInput;
+  private Vector2 _lookInput;
+  private bool _jumpInput;
+  private bool _sprintInput;
 
+  private void Awake()
+  {
+    gameObject.AddComponent<PlayerInput>();
+
+    _player = gameObject.GetOrAddComponent<Player>();
+    _playerInputSystem = new PlayerInputSystem();
+  }
+
+  private void Update()
+  {
+    GetInpup();
+    _player.Move(_moveInput);
+    _player.Jump(_jumpInput);
+    _player.Sprint(_sprintInput);
+    _player.Rotate(_lookInput.x);
+  }
+
+  private void OnEnable()
+  {
+    _playerInputSystem.Enable();
+  }
+
+  private void OnDisable()
+  {
+    _playerInputSystem.Disable();
+  }
+  private void GetInpup()
+  {
+    _moveInput = _playerInputSystem.Player.Move.ReadValue<Vector2>();
+    _lookInput = _playerInputSystem.Player.Look.ReadValue<Vector2>();
+    _jumpInput = _playerInputSystem.Player.Jump.WasPressedThisFrame();
+    _sprintInput = _playerInputSystem.Player.Sprint.IsPressed();
+  }
 }
