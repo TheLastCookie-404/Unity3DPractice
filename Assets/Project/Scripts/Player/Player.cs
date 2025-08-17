148a589c40a804149b00d40475805c0464e7fe64
The Last Cookie <74693705+TheLastCookie-404@users.noreply.github.com>
2025-08-17 22:16:21 +0500

    Slope sliding added

--- Assets/Project/Scripts/Player/Player.cs
+++ Assets/Project/Scripts/Player/Player.cs
@@ -11,9 +11,11 @@ public class Player : MonoBehaviour
   [SerializeField, DefaultValue(5f)] private float _mass;
   [SerializeField, DefaultValue(1.5f)] private float _jumpHeight;
   [SerializeField, DefaultValue(10f)] private float _moveInterpolatoinFactor;
+  [SerializeField, DefaultValue(10f)] private float _slopeSlideSpeed;
 
   private CharacterController _characterController;
   private Vector3 _move;
+  private RaycastHit _raycastHit;
 
   private float _moveSpeed;
 
@@ -26,6 +28,7 @@ public class Player : MonoBehaviour
   {
     ApplyControllerMove(_move);
     Gravity();
+    SlopeSlide();
   }
 
   private void ApplyControllerMove(Vector3 motion)
@@ -45,6 +48,26 @@ public class Player : MonoBehaviour
       // -0.5f is for reducing fluctuations when character is on the ground
       // thats a fix for _characterController.isGrounded
       _move.y = -0.5f;
+  }
+
+  private float GetGroundedAngle(out RaycastHit raycastHit)
+  {
+    Debug.DrawRay(transform.position, Vector3.down, Color.red, 5);
+    Physics.Raycast(transform.position, Vector3.down, out raycastHit, 5);
+    return Vector3.Angle(raycastHit.normal, Vector3.up);
+  }
+
+  private void SlopeSlide()
+  {
+    float groundedAngle = GetGroundedAngle(out _raycastHit);
+
+    // InverseTransformDirection to make slide correct directions
+    _raycastHit.normal = transform.InverseTransformDirection(_raycastHit.normal);
+
+    if (groundedAngle >= _characterController.slopeLimit && _characterController.isGrounded)
+    {
+      _move = Vector3.ProjectOnPlane(Vector3.up * (_move.y * _slopeSlideSpeed), _raycastHit.normal);
+    }
   }
 
   public void Move(Vector2 motionInput)
